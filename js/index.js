4ba1ac8be56bccd35d81e423014db064015c9fb9
SertoriusX <tsvetan.georgiev891@gmail.com>
2025-11-04 00:02:35 +0100

    re

--- js/index.js
+++ js/index.js
@@ -19,46 +19,9 @@ startBtn.addEventListener("click", () => {
     playContainer.classList.remove("d-none");
   }, 600);
 });
-const displayContainer = (optional) => {
-  const backGroundColor = document.querySelector("#colorBox");
-
-  switch (optional) {
-    case "first_image":
-      backGroundColor.style.backgroundImage ="url('./imagenes/first_image.png')";
-      backGroundColor.style.backgroundSize = "cover";
-      backGroundColor.style.backgroundAttachment = "fixed";
-      backGroundColor.style.minHeight = "100vh";
-      backGroundColor.style.backgroundPosition = "center";
-      backGroundColor.style.backgroundRepeat = "no-repeat";
-      backGroundColor.style.backgroundColor = "initial";
-      break;
-    case "second":
-      backGroundColor.style.backgroundImage ="url('./imagenes/thumb-1920-849419.jpg')";
-      backGroundColor.style.backgroundSize = "cover";
-      backGroundColor.style.backgroundPosition = "center";
-      backGroundColor.style.backgroundRepeat = "no-repeat";
-      backGroundColor.style.backgroundColor = "initial";
-      break;
-
-    case "third":
-      backGroundColor.style.backgroundImage = "url('./imagenes/chilled.jpg')";
-      backGroundColor.style.backgroundSize = "cover";
-      backGroundColor.style.backgroundPosition = "center";
-      backGroundColor.style.backgroundRepeat = "no-repeat";
-      backGroundColor.style.backgroundAttachment = "fixed";
-      backGroundColor.style.minHeight = "100vh";
-      backGroundColor.style.backgroundColor = "initial";
-      break;
-
-    case "reset":
-      backGroundColor.style.backgroundImage = "none";
-      backGroundColor.style.backgroundColor = "initial";
-      break;
-  }
-};
 
 // index.js
-module.exports = displayContainer;
+
 
 let board = [];
 let selected = null;
@@ -198,3 +161,44 @@ const resetGame = () => {
 };
 
 createBoard();
+
+
+const displayContainer = (optional) => {
+  const backGroundColor = document.querySelector("#colorBox");
+
+  switch (optional) {
+    case "first_image":
+      backGroundColor.style.backgroundImage ="url('./imagenes/first_image.png')";
+      backGroundColor.style.backgroundSize = "cover";
+      backGroundColor.style.backgroundAttachment = "fixed";
+      backGroundColor.style.minHeight = "100vh";
+      backGroundColor.style.backgroundPosition = "center";
+      backGroundColor.style.backgroundRepeat = "no-repeat";
+      backGroundColor.style.backgroundColor = "initial";
+      break;
+    case "second":
+      backGroundColor.style.backgroundImage ="url('./imagenes/thumb-1920-849419.jpg')";
+      backGroundColor.style.backgroundSize = "cover";
+      backGroundColor.style.backgroundPosition = "center";
+      backGroundColor.style.backgroundRepeat = "no-repeat";
+      backGroundColor.style.backgroundColor = "initial";
+      break;
+
+    case "third":
+      backGroundColor.style.backgroundImage = "url('./imagenes/chilled.jpg')";
+      backGroundColor.style.backgroundSize = "cover";
+      backGroundColor.style.backgroundPosition = "center";
+      backGroundColor.style.backgroundRepeat = "no-repeat";
+      backGroundColor.style.backgroundAttachment = "fixed";
+      backGroundColor.style.minHeight = "100vh";
+      backGroundColor.style.backgroundColor = "initial";
+      break;
+
+    case "reset":
+      backGroundColor.style.backgroundImage = "none";
+      backGroundColor.style.backgroundColor = "initial";
+      break;
+  }
+};
+
+module.exports = displayContainer;
